9eb93ed0a1829247de04e440301193716f3e830e
Stone Daniel <stone.daniel416@gmail.com>
2018-04-17 18:48:26 -0400

    Nodes on path are now green as well as reorganization of code to allow more user input including first attempts at handling weight changes

--- google_map.py
+++ google_map.py
@@ -15,12 +15,14 @@ def recolor(fin):
     #path = [(node, attrs['pi']) for node, attrs in G.nodes.data()]
     finish = fin
     path = []
-
+    nodes = []
     while finish != G.nodes[finish]['pi']:
         path.append((finish, G.nodes[finish]['pi']))
+        nodes.append((finish))
         finish = G.nodes[finish]['pi']
-    print("path = ", path)
+    #print("path = ", path)
     nx.draw_networkx_edges(G, pos, edgelist=path, edge_color='green')
+    nx.draw_networkx_nodes(G, pos, nodelist = nodes, node_color='green')
 
 
 def display():
@@ -63,34 +65,61 @@ def dijkstra(start):
 
 
 G = nx.Graph()
-G.add_node('a', d=sys.maxsize, pi=None, color='r', visited=True)
-G.add_node('b', d=sys.maxsize, pi=None, color='r', visited=True)
-G.add_node('c', d=sys.maxsize, pi=None, color='r', visited=True)
-G.add_node('d', d=sys.maxsize, pi=None, color='r', visited=True)
-G.add_node('e', d=sys.maxsize, pi=None, color='r', visited=True)
+edges = [
+        ("A", "B", 3),
+        ("A", "D", 6),
+        ("B", "C", 1),
+        ("B", "D", 3),
+        ("B", "E", 5),
+        ("C", "E", 6),
+        ("D", "E", 5),
+        ("D", "F", 9),
+        ("E", "F", 1),
+        ("E", "G", 1),
+        ("F", "G", 2)
+]
 
-G.add_edge('a', 'b', weight=4, color='r')
-G.add_edge('a', 'c', weight=3, color='r')
-G.add_edge('a', 'd', weight=2, color='r')
-G.add_edge('c', 'd', weight=5, color='r')
-G.add_edge('b', 'c', weight=2, color='r')
-G.add_edge('b', 'e', weight=9, color='r')
+for (e1, e2, w) in edges:
+    G.add_node(e1, d=sys.maxsize, pi=None, color='r', visited=True)
+    G.add_node(e2, d=sys.maxsize, pi=None, color='r', visited=True)
+    G.add_edge(e1, e2, weight=w, color='r')
+
 pos = nx.spring_layout(G)
-start = 'e'
-end = 'd'
+start = input('Please enter start node')
+end = input('Please enter finish node')
+temp = end
 dijkstra(start)
-path_to_finish = []
-path_to_finish.append(end)
-while start != end:
-    path_to_finish.append(G.nodes[end]['pi'])
-    end = G.nodes[end]['pi']
-path_to_finish.reverse()
-print(path_to_finish)
 
-end = 'd'
+def get_path(begin, dest):
+    path = []
+    path.append(end)
+    while begin != dest:
+        path.append(G.nodes[dest]['pi'])
+        dest = G.nodes[dest]['pi']
+    path.reverse()
+    return path
+
+
+# path_to_finish = []
+# path_to_finish.append(end)
+# while start != temp:
+#     path_to_finish.append(G.nodes[temp]['pi'])
+#     temp = G.nodes[temp]['pi']
+# path_to_finish.reverse()
+# print(path_to_finish, G.nodes[end]['d'])
+
+path_to_finish = get_path(start, end)
 while start != end and path_to_finish:
     start = path_to_finish[0]
     del path_to_finish[0]
+    G.nodes[start]['color'] = 'blue'
+    to_change = input('Change an edge?')
+    if(to_change == 'yes'):
+        edge1 = input('Input edge1: ')
+        edge2 = input('Input edge2: ')
+        w = input('Input Updated weight:')
+        G[edge1][edge2]['weight'] = int(w)
+        path_to_finish = get_path(start, end)
     dijkstra(start)
     display()
     recolor(end)
